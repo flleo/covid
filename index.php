281c72180081850736a6227a1bd49471e970e79a
Fede <fedelleos@gmail.com>
2020-12-17 19:08:42 +0000

    index.php

---
+++ index.php
@@ -0,0 +1,56 @@
+<php?
+
+?>
+
+<html lang="en"><head></head><body><nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand"><img id="logo" src="images/covid.jpeg"></a>
+    <div class="row ">
+        <div class="form-inline ">
+            <a class="form-control mr-sm-2" aria-label="Search">Login</a>
+            <a class="form-control mr-sm-2" aria-label="Search">Salir</a>
+        </div>
+    </div>
+</nav>
+
+
+  
+    <title>covid</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/css.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+  
+      <div class="container d-flex justify-content-center p-5">
+          
+          <div id="login-box" class="col-md-6 >
+            <form id=" login-form"="" action="" method="post">
+                <h3 class="text-center text-info">Login</h3>
+                <div class="form-group">
+                    <label for="username" class="text-info">Username:</label><br>
+                    <input type="text" name="username" id="username" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="password" class="text-info">Password:</label><br>
+                    <input type="text" name="password" id="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="remember-me" class="text-info"><span>Remember me</span>&nbsp;<span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                    <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                </div>
+                <div id="register-link" class="text-right">
+                    <a href="#" class="text-info" marked="1">Register here</a>
+                </div>
+            
+        </div>
+      </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+</body></html>
